f8894cd70aa72503385aba2fca61e4e85dabef91
Welly Tambunan <if05041@gmail.com>
2018-06-24 19:01:39 +0700

    adding challenge detail for a given problem

---
+++ StartProcess/Challenge.cs
@@ -0,0 +1,24 @@
+﻿namespace StartProcess
+{
+    public class Challenge
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ProblemStatement { get; set; }
+        public string InputFormat { get; set; }
+        public string Constraint { get; set; }
+        public string OutputFormat { get; set; }
+        public string Tags { get; set; }
+        public Difficulty Difficulty { get; set; }
+    }
+
+    public enum Difficulty
+    {
+        Easy, 
+        Medium, 
+        Hard, 
+        Advanced, 
+        Expert
+    }
+    
+}
