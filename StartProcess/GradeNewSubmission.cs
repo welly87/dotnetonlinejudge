5c76f6091ae8c914158dbca4764af4798c6351ff
Welly Tambunan <if05041@gmail.com>
2018-06-24 18:37:24 +0700

    remove unused using and space

--- StartProcess/GradeNewSubmission.cs
+++ StartProcess/GradeNewSubmission.cs
@@ -5,7 +5,6 @@ namespace StartProcess
     public class GradeNewSubmission
     {
         public Guid Id { get; set; }
-
         public string SourceCode { get; set; }
         public int ProblemId { get; internal set; }
     }
